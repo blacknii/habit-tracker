1bf0462b30f847967fd55e28a1645220cc3c9336
Dominik Stanczyk <stanczykdominik0@gmail.com>
2023-03-03 18:34:47 +0100

    buttons rendering

--- habit tracker/src/components/dashboard/dashboardLeft/DasboardLeftWeek.jsx
+++ habit tracker/src/components/dashboard/dashboardLeft/DasboardLeftWeek.jsx
@@ -1,56 +1,66 @@
-import React from 'react';
-import styles from './DasboardLeftWeek.module.css';
-import DashboardLeftHabit from './DashboardLeftHabit';
+import React from "react";
+import styles from "./DasboardLeftWeek.module.css";
+import DashboardLeftHabit from "./DashboardLeftHabit";
 import { useDispatch, useSelector } from "react-redux";
-import {newHabit, removeHabit} from "../../../redux/habits";
-
+import { newHabit, removeHabit } from "../../../redux/habits";
 
 function DasboardLeftWeek() {
-  const { listOfHabits } = useSelector((state)=> state.habits)
+  const { listOfHabits } = useSelector((state) => state.habits);
   const dispatch = useDispatch();
 
-  let curr = new Date()
-  let dayOfTheWeek = curr.getDay() ? curr.getDay() : 7
+  let curr = new Date();
+  let dayOfTheWeek = curr.getDay() ? curr.getDay() : 7;
 
-
-  return <div className={styles.container}>
-    <div className={styles["main-top"]}>
-      <div>
-        <button>◀</button>
-        <button>▶</button>
-      </div>
-      <h2>Mon, Feb 20 - Sun, Feb 26</h2>
-      <div>
-        <button>1️⃣</button>
-        <button>2️⃣</button>
-      </div>
-    </div>
-    <div className={styles["overall-progress"]}>
-      <p>progress bar</p>
-      <div className={styles["overall-progress-percentage-comparison"]}>
-      <p>🔽 Down 25% from the week before</p>
-      <p>75% achieved</p>
-      </div>
-    </div>
-    <div>
-      <div className={styles.week}>
-        <div></div>
-        <div className={styles.days}>
-          <div className={styles.day}>Tue</div>
-          <div className={styles.day}>Mon</div>
-          <div className={styles.day}>Wen</div>
-          <div className={styles.day}>Thu</div>
-          <di className={styles.day}v>Fri</di>
-          <div className={styles.day}>Sat</div>
-          <div className={styles.day}>Sun</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles["main-top"]}>
+        <div>
+          <button>◀</button>
+          <button>▶</button>
         </div>
-          <div></div>
+        <h2>Mon, Feb 20 - Sun, Feb 26</h2>
+        <div>
+          <button>1️⃣</button>
+          <button>2️⃣</button>
+        </div>
       </div>
-      {listOfHabits.map(habbit => {
-          return <DashboardLeftHabit key={habbit.name} name={habbit.name} startDay={habbit.startDay} lastWeek={habbit.lastWeek} dayOfTheWeek={dayOfTheWeek}/>
+      <div className={styles["overall-progress"]}>
+        <p>progress bar</p>
+        <div className={styles["overall-progress-percentage-comparison"]}>
+          <p>🔽 Down 25% from the week before</p>
+          <p>75% achieved</p>
+        </div>
+      </div>
+      <div>
+        <div className={styles.week}>
+          <div></div>
+          <div className={styles.days}>
+            <div className={styles.day}>Tue</div>
+            <div className={styles.day}>Mon</div>
+            <div className={styles.day}>Wen</div>
+            <div className={styles.day}>Thu</div>
+            <di className={styles.day} v>
+              Fri
+            </di>
+            <div className={styles.day}>Sat</div>
+            <div className={styles.day}>Sun</div>
+          </div>
+          <div></div>
+        </div>
+        {listOfHabits.map((habbit) => {
+          return (
+            <DashboardLeftHabit
+              key={habbit.name}
+              name={habbit.name}
+              startDay={habbit.startDay}
+              lastWeek={habbit.lastWeek}
+              dayOfTheWeek={dayOfTheWeek}
+            />
+          );
         })}
+      </div>
     </div>
-  </div>
-};
+  );
+}
 
 export default DasboardLeftWeek;
--- habit tracker/src/components/dashboard/dashboardRight/DashboardRightHabit.jsx
+++ habit tracker/src/components/dashboard/dashboardRight/DashboardRightHabit.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import styles from './DashboardRightHabit.module.css';
+import React from "react";
+import styles from "./DashboardRightHabit.module.css";
 import { useDispatch } from "react-redux";
-import {completionsSwitch} from "../../../redux/habits"
-
+import { completionsSwitch } from "../../../redux/habits";
 
 function DashboardRightHabit(props) {
   const dispatch = useDispatch();
-  const lastIndex = props.lastWeek.length - 1
-  const todaysTask = props.lastWeek[lastIndex]
-  const uncompletedTask = <div className={styles.submit}><button onClick={() => dispatch(completionsSwitch([props.name, lastIndex]))}>Mark Complete</button></div>
-  const completedTask = <div className={styles.submit}><p>✅Completed</p><button onClick={() => dispatch(completionsSwitch([props.name, lastIndex]))}>Undo</button></div>
+  const lastIndex = props.lastWeek.length - 1;
+  const todaysTask = props.lastWeek[lastIndex];
+  const uncompletedTask = (
+    <div className={styles.submit}>
+      <button
+        onClick={() => dispatch(completionsSwitch([props.name, lastIndex]))}
+      >
+        Mark Complete
+      </button>
+    </div>
+  );
+  const completedTask = (
+    <div className={styles.submit}>
+      <p>✅Completed</p>
+      <button
+        onClick={() => dispatch(completionsSwitch([props.name, lastIndex]))}
+      >
+        Undo
+      </button>
+    </div>
+  );
 
-  return <div className={styles.container}>
-    <p>{props.name}</p>
-    {todaysTask ? completedTask : uncompletedTask}
-  </div>
-};
+  return (
+    <div className={styles.container}>
+      <p>{props.name}</p>
+      {todaysTask ? completedTask : uncompletedTask}
+    </div>
+  );
+}
 
 export default DashboardRightHabit;
