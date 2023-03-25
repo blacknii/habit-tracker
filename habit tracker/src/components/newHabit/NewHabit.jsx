58464c9596241be450ff7ac6708189d314cb299d
Dominik Stanczyk <stanczykdominik0@gmail.com>
2023-03-25 14:54:55 +0100

    form styling

--- habit tracker/src/components/newHabit/NewHabit.jsx
+++ habit tracker/src/components/newHabit/NewHabit.jsx
@@ -7,28 +7,37 @@ function NewHabit() {
       <div className={styles.overlay}></div>
       <div className={styles.container}>
         <div>
-          <div className={styles.test}>
-            <p>Add Habit</p>
-            <button className={styles.button}>x</button>
+          <div className={styles["display-flex"]}>
+            <h2>Add Habit</h2>
+            <button className={styles["x-button"]}>x</button>
           </div>
           <p>Tackle your goals in daily doses</p>
         </div>
         <form className={styles.form}>
           <div>
-            <label for="name">1. Name this habit</label>
+            <label className={styles["label-big"]} for="name">
+              1. Name this habit
+            </label>
             <br />
-            <input type="text" id="name" name="name" />
+            <input
+              className={styles["input-text"]}
+              type="text"
+              id="name"
+              name="name"
+            />
           </div>
           <div>
-            <label for="type">2. Habit Type</label>
+            <label className={styles["label-big"]} for="type">
+              2. Habit Type
+            </label>
             <br />
-            <div className={styles.test}>
-              <div className={styles.test}>
+            <div className={styles["habit-type"]}>
+              <div className={styles["display-flex"]}>
                 <input type="radio" id="type" name="type" />
                 <label for="type">To-Do</label>
               </div>
               <br />
-              <div className={styles.test}>
+              <div className={styles["display-flex"]}>
                 <input type="radio" id="type" name="type" />
                 <label for="type">Not-To-Do</label>
                 <br />
@@ -36,53 +45,59 @@ function NewHabit() {
             </div>
           </div>
           <div>
-            <label for="frequency">3. Weekly frequency</label>
-            <div className={styles.test}>
+            <label className={styles["label-big"]} for="frequency">
+              3. Weekly frequency
+            </label>
+            <div className={styles["display-label-checkbox"]}>
               <br />
-              <label for="frequency">
+              <label className={styles["label-checkbox"]} for="frequency">
                 Mon
                 <input type="checkbox" />
               </label>
               <br />
-              <label for="frequency">
+              <label className={styles["label-checkbox"]} for="frequency">
                 Tue
                 <input type="checkbox" />
               </label>
               <br />
-              <label for="frequency">
+              <label className={styles["label-checkbox"]} for="frequency">
                 Wed
                 <input type="checkbox" />
               </label>
               <br />
-              <label for="frequency">
+              <label className={styles["label-checkbox"]} for="frequency">
                 Thu
                 <input type="checkbox" />
               </label>
               <br />
-              <label for="frequency">
+              <label className={styles["label-checkbox"]} for="frequency">
                 Fri
                 <input type="checkbox" />
               </label>
               <br />
-              <label for="frequency">
+              <label className={styles["label-checkbox"]} for="frequency">
                 Sat
                 <input type="checkbox" />
               </label>
               <br />
-              <label for="frequency">
+              <label className={styles["label-checkbox"]} for="frequency">
                 Sun
                 <input type="checkbox" />
               </label>
               <br />
             </div>
-            <div className={styles.test}>
-              <button className={styles.button}>Week days</button>
-              <button className={styles.button}>Every day</button>
+            <div className={styles["display-weekly-frequency-button"]}>
+              <button className={styles["weekly-frequency-button"]}>
+                Week days
+              </button>
+              <button className={styles["weekly-frequency-button"]}>
+                Every day
+              </button>
             </div>
           </div>
-          <div className={styles.test}>
-            <button className={styles.button}>Add Habit</button>
-            <button className={styles.button}>+Add Another</button>
+          <div className={styles["display-form-button"]}>
+            <button className={styles["form-button"]}>Add Habit</button>
+            <button className={styles["form-button"]}>+Add Another</button>
           </div>
         </form>
       </div>
