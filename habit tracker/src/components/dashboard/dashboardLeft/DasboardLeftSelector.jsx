5687c940e030ece150e4b9523da30bc1e3a33d1e
Dominik Stanczyk <stanczykdominik0@gmail.com>
2023-04-02 20:42:37 +0200

    style changes

--- habit tracker/src/components/dashboard/dashboardLeft/DasboardLeftSelector.jsx
+++ habit tracker/src/components/dashboard/dashboardLeft/DasboardLeftSelector.jsx
@@ -1,23 +1,49 @@
 import React from "react";
 import styles from "./DasboardLeftSelector.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { datetypeChanger } from "../../../redux/habits";
 import { modalSwitch } from "../../../redux/newHabit";
 
 function DasboardLeftSelector() {
   const dispatch = useDispatch();
-
+  const State = useSelector((state) => state.habits);
+  const type = State.timePeriod.type;
+  console.log(type);
   return (
     <div className={styles.container}>
       <ul className={styles["date-range-selector"]}>
-        <button onClick={() => dispatch(datetypeChanger("Week"))}>Week</button>
-        <button onClick={() => dispatch(datetypeChanger("Month"))}>
+        <li
+          className={
+            type == "Week" ? styles["button-picked"] : styles["button"]
+          }
+          onClick={() => dispatch(datetypeChanger("Week"))}
+        >
+          Week
+        </li>
+        <li
+          className={
+            type == "Month" ? styles["button-picked"] : styles["button"]
+          }
+          onClick={() => dispatch(datetypeChanger("Month"))}
+        >
           Month
-        </button>
-        <button onClick={() => dispatch(datetypeChanger("Year"))}>Year</button>
-        <button onClick={() => dispatch(datetypeChanger("AllTime"))}>
+        </li>
+        <li
+          className={
+            type == "Year" ? styles["button-picked"] : styles["button"]
+          }
+          onClick={() => dispatch(datetypeChanger("Year"))}
+        >
+          Year
+        </li>
+        <li
+          className={
+            type == "AllTime" ? styles["button-picked"] : styles["button"]
+          }
+          onClick={() => dispatch(datetypeChanger("AllTime"))}
+        >
           All Time
-        </button>
+        </li>
       </ul>
       <button onClick={() => dispatch(modalSwitch(true))}>+ Add Habbit</button>
     </div>
