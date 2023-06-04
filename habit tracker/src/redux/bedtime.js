a86494e4484df9946da0ac01993f481fb3d0f854
blacknii <42908977+blacknii@users.noreply.github.com>
2023-06-04 16:22:42 +0200

    Refactor: Improve readability of Redux slice

--- habit tracker/src/redux/bedtime.js
+++ habit tracker/src/redux/bedtime.js
@@ -1,13 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const bedtime =
-  localStorage.getItem("bedtime") != null
-    ? localStorage.getItem("bedtime")
-    : null;
+const DEFAULT_BEDTIME_MESSAGE = "Add Your Bedtime";
+
+const bedtime = localStorage.getItem("bedtime");
 
 const initialState = {
-  bedtime: bedtime,
-  bedtimeMessage: "Add Your Bedtime",
+  bedtime,
+  bedtimeMessage: DEFAULT_BEDTIME_MESSAGE,
   isModalVisible: false,
 };
 
@@ -16,46 +15,21 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     setBedtime: (state, action) => {
-      if (action.payload === 0) {
+      const newBedtime = action.payload;
+
+      if (newBedtime === 0) {
         localStorage.removeItem("bedtime");
         state.bedtime = null;
       } else {
-        state.bedtime = action.payload;
-        localStorage.setItem("bedtime", state.bedtime);
+        state.bedtime = newBedtime;
+        localStorage.setItem("bedtime", newBedtime);
       }
     },
     setBedtimeMessage: (state) => {
       if (state.bedtime === null) {
-        state.bedtimeMessage = "Add Your Bedtime";
+        state.bedtimeMessage = DEFAULT_BEDTIME_MESSAGE;
       } else {
-        const inputString = localStorage.getItem("bedtime");
-
-        const [timeStr, period] = inputString.split(" ");
-        let [hours, minutes] = timeStr.split(":");
-        // Convert hours to 24-hour format if necessary
-        if (period === "PM" && hours !== "12") {
-          hours = parseInt(hours, 10) + 12;
-        } else if (period === "AM" && hours === "12") {
-          hours = "00";
-        }
-        // Create a new Date object for the bedtime
-        const bedtime = new Date();
-        bedtime.setHours(parseInt(hours, 10));
-        bedtime.setMinutes(parseInt(minutes, 10));
-        // Get the current time
-        const now = new Date();
-        // Calculate the difference in hours and minutes
-        const diffMs = bedtime.getTime() - now.getTime();
-        const diffHrs = Math.floor(diffMs / (1000 * 60 * 60));
-        const diffMins = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
-        // Check if it's time to sleep
-        if (diffHrs <= 0 && diffMins <= 0) {
-          state.bedtimeMessage = "Time to sleep";
-        } else {
-          // Format the result into the desired string format
-          const result = `${diffHrs} hrs ${diffMins} mins till bedtime`;
-          state.bedtimeMessage = result;
-        }
+        state.bedtimeMessage = calculateBedtimeMessage();
       }
     },
     setBedtimeModal: (state, action) => {
@@ -63,6 +37,36 @@ export const counterSlice = createSlice({
     },
   },
 });
+
+function calculateBedtimeMessage() {
+  const inputString = localStorage.getItem("bedtime");
+
+  let [timeStr, period] = inputString.split(" ");
+  let [hours, minutes] = timeStr.split(":");
+
+  // Convert hours to 24-hour format if necessary
+  if (period === "PM" && hours !== "12") {
+    hours = parseInt(hours, 10) + 12;
+  } else if (period === "AM" && hours === "12") {
+    hours = "00";
+  }
+
+  const bedtime = new Date();
+  bedtime.setHours(parseInt(hours, 10));
+  bedtime.setMinutes(parseInt(minutes, 10));
+
+  const now = new Date();
+
+  const diffMs = bedtime.getTime() - now.getTime();
+  const diffHrs = Math.floor(diffMs / (1000 * 60 * 60));
+  const diffMins = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+
+  if (diffHrs <= 0 && diffMins <= 0) {
+    return "Time to sleep";
+  }
+
+  return `${diffHrs} hrs ${diffMins} mins till bedtime`;
+}
 
 export const { setBedtime, setBedtimeMessage, setBedtimeModal } =
   counterSlice.actions;
